4c2548a1306578ab347ae72288bac5ad4996b366
Nguyễn Quyết <52320888+QuyetNX-dev@users.noreply.github.com>
2020-04-25 16:30:03 +0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Transection-library
-Created with CodeSandbox
---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const shortid = require("shortid");
+
+const db = require("./db");
+const updateRoute = require("./routes/update.route");
+const deleteRoute = require("./routes/delete.route");
+const postRoute = require("./routes/post.route");
+
+const updateUserRoute = require("./routes/users/update.route");
+const deleteUserRoute = require("./routes/users/delete.route");
+const postUserRoute = require("./routes/users/post.route");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+// const transection = [];
+app.get("/", (req, res) => {
+  res.render("myFibrary");
+});
+
+app.get("/book", (req, res) => {
+  db.get("todo")
+    .forEach((item, index) => {
+      item.stt = index + 1;
+    })
+    .write();
+  res.render("index.pug", {
+    todos: db.get("todo").value()
+  });
+});
+
+app.get("/users", (req, res) => {
+  db.get("users")
+    .forEach((item, index) => {
+      item.stt = index + 1;
+    })
+    .write();
+  res.render("users/index", {
+    users: db.get("users").value()
+  });
+});
+
+app.get("/transection", (req, res) => {
+  db.get("transection").forEach(item => {
+    item.stt = db.get("transection").value().length + 1;
+  });
+  res.render("transection/index", {
+    transection: db.get("transection")
+  });
+});
+
+app.get("/transection/create", (req, res) => {
+  res.render("transection/create", {
+    users: db.get("users").value(),
+    books: db.get("todo").value()
+  });
+});
+
+app.post("/transection/create", (req, res) => {
+  req.body.id = shortid.generate();
+  req.body.stt = db.get("todo").value().length + 1;
+  db.get("transection")
+    .push(req.body.id)
+    .write();
+  res.redirect("/transection");
+});
+
+app.use("/book/update", updateRoute);
+app.use("/book/delete", deleteRoute);
+app.use("/book/post", postRoute);
+
+app.use("/users/update", updateUserRoute);
+app.use("/users/delete", deleteUserRoute);
+app.use("/users/post", postUserRoute);
+
+app.listen(PORT, () => {
+  console.log("Service running on PORT:" + PORT);
+});
